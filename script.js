919af649cffae2ef2eaf5689252b82a9321b24e3
dimitarpapalazov <d.papalazov@outlook.com>
2022-03-18 11:19:57 +0100

    Update script.js
    test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function jump(li) {
-    id = li.getAttribute("value") + "-container";
-    document.getElementById(id).scrollIntoView();
-}
+//function jump(li) {
+  //  id = li.getAttribute("value") + "-container";
+    //document.getElementById(id).scrollIntoView();
+//}
