cbf58c11fc84cc1d5b4c6f1bb834e71369d22186
dimitarpapalazov <d.papalazov@outlook.com>
2025-02-06 19:14:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function jump(li) {
     id = li.getAttribute("value") + "-container";
     document.getElementById(id).scrollIntoView();
 }
+
+const element = document.getElementById('todaysYear');
+element.innerText = new Date().getFullYear();
